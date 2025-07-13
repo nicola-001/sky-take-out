82f0d397c98691d0b30cf6c053c3cfc941aae98f
Nicola <2629137145@qq.com>
2025-07-13 10:08:27 +0800

    编辑员工查询代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -144,6 +144,12 @@ public class EmployeeController {
         return Result.success(employee);
     }
 
+    /**
+     * 编辑员工信息
+     *
+     * @param employeeDTO
+     * @return
+     */
     @PutMapping
     @ApiOperation("编辑员工信息")
     public Result update(@RequestBody EmployeeDTO employeeDTO) {
